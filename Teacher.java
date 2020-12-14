ce57a76bf73b5d942ddbb7279666e5a0d01d9c40
Natalia-01 <t8190131@aueb.gr>
2020-12-14 17:55:44 +0200

    βελτιωση με λιστες και μεθοδος findTeacher

--- Teacher.java
+++ Teacher.java
@@ -1,22 +1,25 @@
 package covid;
 
+import java.util.ArrayList;
+
 public class Teacher {
 	 
 	private String surname;
 	private String name;
 	private int id;
-	protected Class classes[] = new Class [100];
-	private int phonenumber;
+	private String phonenumber;
 	private String gender;
-	private String status = "negative";
+	private String status = "negative"; //αρχικοποιει το αν ενας καθηγητης εχει κορονα.
+	protected static ArrayList <Teacher> allteachers = new ArrayList<Teacher>();
 	
-	public Teacher(String surname, String name, int id, int phonenumber, String gender) {
+	public Teacher(String surname, String name, int id, String phonenumber, String gender) {
 		super();
 		this.surname = surname;
 		this.name = name;
 		this.id = id;
 		this.phonenumber = phonenumber;
 		this.gender = gender;
+		allteachers.add(this);
 	}
 
 	public String getStatus() {
@@ -51,11 +54,11 @@ public class Teacher {
 		this.id = id;
 	}
 
-	public int getPhonenumber() {
+	public String getPhonenumber() {
 		return phonenumber;
 	}
 
-	public void setPhonenumber(int phonenumber) {
+	public void setPhonenumber(String phonenumber) {
 		this.phonenumber = phonenumber;
 	}
 
@@ -69,7 +72,26 @@ public class Teacher {
 
 	@Override
 	public String toString() {
-		return "Teacher [surname=" + surname + ", name=" + name + ", id=" + id + "]";
+		return "Teacher [surname=" + surname + ", name=" + name + ", id=" + id + ", status=" + status +"]";
+	}
+	
+	public static Teacher findTeacher(int code) {
+		boolean flag = false;
+		int i = 0;
+		int position = -1;
+		while (i < allteachers.size() && flag == false) {
+			if ( allteachers.get(i).getId() == code) {
+				flag = true;
+				position = i;
+			}
+			i++;
+		}
+		if (flag == true) {
+			return allteachers.get(position);
+		} else {
+			return null;
+		}
+		
 	}
 	
 	
