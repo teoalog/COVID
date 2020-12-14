b5584c88dce3407fbe73f51443789f6971b98a6c
katetanti <72856891+katetanti@users.noreply.github.com>
2020-12-14 14:09:31 +0200

    εισαγωγη λιστας και βελτιωση findHospital

--- Hospital.java
+++ Hospital.java
@@ -1,30 +1,49 @@
-
+import java.util.ArrayList;
 public class Hospital {
 
-	public String nameofhospital;
-	public char area;
-	protected static Hospital [] allhospitals = new Hospital[100000]; /*alla hospitals are saved in an array*/
+	private String nameofhospital;
+	private char area;
+	protected static ArrayList<Hospital> allhospitals = new ArrayList<Hospital>(); /*all hospitals are saved in an array list*/
 	private static int count; /*counts the number of hospitals*/
 	
-
 	public Hospital(String nameofhospital, char area) {
 		this.nameofhospital = nameofhospital;
 		this.area =  area;
-		allhospitals[count] = this;
+		allhospitals.set(count, this);
 		count++;
+	}
+	 
+	public String getNameofhospital() {
+		return nameofhospital;
+	}
+	
+	public void setNameofhospital() {
+		this.nameofhospital = nameofhospital;
+	}
+	
+	public char getArea() {
+		return area;
+	}
+	
+	public void setArea() {
+		this.area = area;
+	}
+	
+	public String toString() {
+		return nameofhospital + " " + area;
 	}
 	
 	/*finds the nearest hospital for the patient*/
-	public static void findHospital(int area) {
+	public static void findHospital(int schoolid) {
 		boolean flag = false;
-	for	(int i = 0; i < allhospitals.length; i++) {
-		if (allhospitals[i].area == area) {
-			System.out.println(allhospitals[i].area);
+	for	(int i = 0; i < allhospitals.size(); i++) {
+		if (allhospitals.get(i).area == School.allschools.get(schoolid).getArea()) {
+			System.out.println("Το όνομα του νοσοκομείου αναφοράς στο δήμο σας είναι: " + allhospitals.get(i).nameofhospital);
 			flag = true;
 		}
 	}	
 	if (flag == true) {
-		System.out.println("nosokomeio se allo dhmo");
+		System.out.println("Δεν υπάρχει νοσοκομείο αναφοράς στο δήμο που βρίσκεται το σχολείο σας");
 	}
 	}
 }
