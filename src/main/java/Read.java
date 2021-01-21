02fdc488e6a266a3c3b80c74d0c45caa510fddc6
Thodoris Alogogiannis <t8190006@aueb.gr>
2021-01-21 23:08:41 +0200

    Update pieces of code.

--- src/main/java/Read.java
+++ src/main/java/Read.java
@@ -2,60 +2,54 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
-import java.nio.file.Path;//interface 
+import java.nio.file.Path; //interface 
 import java.nio.file.Paths;
 
-/* Through this class we manage to read data from files (for students, schools, passwords etc) */
+/* With this class we manage to read data from files (for students, schools, passwords etc) */
 
 public class Read { 
 	static int f = 1;
 	static int position_s = 0;
 	static int position_t = 0;
-	protected static void readText(String filename,String choice) {
-		Path pathtoFile = Paths.get(filename);// we create a Path object from the file's URI to trace it.    
+	protected static void readText(String filename, String choice) {
+		Path pathToFile = Paths.get(filename); // we create a Path object from the file's URI to trace it.    
 		try {
-			BufferedReader br = Files.newBufferedReader(pathtoFile, StandardCharsets.UTF_8);//we create a BufferedReader object so that 
-			                                                                                //we'll be able to read one by one 
-			                                                                               //the lines of our file.
-			String line = br.readLine();// it reads the first line.
-			while(line != null) {//as long as the file has more lines we repeat. 
-				if(choice.equals("Student")){   //if its the students file. 
-					String ar[] = line.split(",", 7);  // we split the elements of each line 
-					                                  // based on the commas and we put each 
-					                                 // element in an array
+			BufferedReader br = Files.newBufferedReader(pathToFile, StandardCharsets.UTF_8); /* we create a BufferedReader object so that 
+			                                                                                    we'll be able to read one by one 
+			                                                                                    the lines of our file.*/
+			String line = br.readLine(); //reads the first line.
+			while (line != null) { //as long as the file has more lines, we repeat. 
+				if (choice.equals("Student")){   //if it's the students file. 
+					String ar[] = line.split(",", 7);  /* we split the elements of each line 
+					                                      based on the commas and we put each 
+					                                      element in an array */
 					Student s = new Student(Integer.parseInt(ar[0]),ar[1],ar[2],ar[3],ar[4],Integer.parseInt((ar[5]), Integer.parseInt(ar[6]))); //��������� ��� ����������� Student.
 					System.out.println(s);
-				}else if(choice.contentEquals("Teacher")){// the teachers file. 
+				} else if (choice.contentEquals("Teacher")){ // the teachers file. 
 					String ar[] = line.split(",", 6);
 					Teacher t = new Teacher(ar[1],ar[2],Integer.parseInt(ar[0]),ar[3],ar[4],Integer.parseInt(ar[5])); 
 					System.out.println(t);
-				} else if(choice.contentEquals("Class")) { // the classes file
+				} else if (choice.contentEquals("Class")) { // the classes file
 					// for the classes.
 					
-			    }else if(choice.contentEquals("Grade")) { // the grades file
+			    } else if (choice.contentEquals("Grade")) { // the grades file
 					
 					
-				}else if(choice.contentEquals("School")) {//the schools file
+				} else if (choice.contentEquals("School")) { //the schools file
 				
-				}else if(choice.contentEquals("Municipality")) {//the municipalities file
+				} else if(choice.contentEquals("Municipality")) { //the municipalities file
 					String ar[] = line.split(",", 3);
 					Municipality m = new Municipality(ar[0],Integer.parseInt(ar[1]),Integer.parseInt(ar[2]));
 					System.out.println(m);	
-				}else if(choice.contentEquals("Hospital")) {
+				} else if (choice.contentEquals("Hospital")) {
 					String ar[] = line.split("," , 2);
 					Hospital h = new Hospital(ar[0],ar[1]);
 					System.out.println(h);				
 				}
-				line = br.readLine(); // read the next line
-		}	    
-			}catch (IOException e) {
-				e.printStackTrace();
-			}
+				line = br.readLine(); //read the next line
+			}	    
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
-	
 }
-
-
-	
-
-
