d85d7ccd32ba01ac4582bccb80d106a7e994c48b
Thodoris Alogogiannis <t8190006@aueb.gr>
2021-01-21 22:39:59 +0200

    Correct little things.

--- src/main/java/Grade.java
+++ src/main/java/Grade.java
@@ -3,10 +3,9 @@ import java.util.ArrayList;
 public class Grade {
 	
 	private int id;
-	private int cases=0; // the active cases of covid in this grade
+	private int cases = 0; // the active cases of covid in this grade
 	protected ArrayList<Class> classes = new ArrayList<Class>(); // a list with the classes each grade has.
 	protected static ArrayList<Grade> allgrades = new ArrayList<Grade>();
-	
 	
 	public Grade(int id) {
 		super();
@@ -19,7 +18,7 @@ public class Grade {
 	}
 	
 	public void setCases() {
-		for(int i = 0; i < classes.size(); i++) {
+		for (int i = 0; i < classes.size(); i++) {
 			cases += classes.get(i).getCases();
 		}
 	}
@@ -28,21 +27,9 @@ public class Grade {
 		return id;
 	}
 
-	public void setName(int id) {
-		this.id = id;
-	}
-
 	@Override
 	public String toString() {
 		String s = "Grade";
-		return "ID: " + id + "/n Cases: " + cases;
+		return "ID: " + id + "\nCases: " + cases;
 	}
-	
-	
-	
-    
-	
-	
-	
-
 }
