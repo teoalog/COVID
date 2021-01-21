e4073a78e4d3199e66e16082894505a9f1d53b15
Thodoris Alogogiannis <t8190006@aueb.gr>
2021-01-21 23:09:57 +0200

    Change file name and update some lines.

--- src/main/java/RelatedToEdu.java
+++ src/main/java/RelatedToEdu.java
@@ -1,13 +1,13 @@
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-public class RelatedwithEdu extends JFrame {
+public class RelatedToEdu extends JFrame {
 
 	private JPanel pedu;
 	private JFrame fedu;
 
 	/* Create the frame. */
-	public RelatedwithEdu() {
+	public RelatedToEdu() {
 		pedu = new JPanel();
 		fedu = new JFrame();
 		fedu.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
@@ -16,7 +16,6 @@ public class RelatedwithEdu extends JFrame {
 		fedu.setTitle("Information about SARS-CoV-2 related to Education");
 		fedu.getContentPane().add(pedu);
 		pedu.setLayout(null);
-
 	}
-	/* Show information for COVID-19 related with Education. */
+	/* Show information for COVID-19 related to Education. */
 }
