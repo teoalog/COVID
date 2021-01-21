042f776e3c1a9695a51c3128db1a7c633bec9895
Thodoris Alogogiannis <t8190006@aueb.gr>
2021-01-21 23:02:32 +0200

    Make a few changes

--- src/main/java/Municipality.java
+++ src/main/java/Municipality.java
@@ -2,68 +2,61 @@ import java.util.ArrayList;
 
 public class Municipality {
 	
-	protected ArrayList<School> schools = new ArrayList<School>();
-    private String municipality;
-    private int casesinmunicipality = 0;
-    private int closedschools = 0;
+	private String municipality;
+    private int casesInMunicipality = 0;
+    private int closedSchools = 0;
     private static int count = 0;
-    protected static ArrayList<Municipality> allmunicipalities = new ArrayList<Municipality>();
+    protected ArrayList<School> schools = new ArrayList<School>();
+    protected static ArrayList<Municipality> allMunicipalities = new ArrayList<Municipality>();
     
-    	public Municipality(String municipality, int casesinmunicipality, int closedschools ) {
-    		this.municipality = municipality;
-    		this.casesinmunicipality = casesinmunicipality;
-    		this.closedschools = closedschools;
-    		allmunicipalities.set(count,this);
-    		count ++;
-    	}
+    public Municipality(String municipality, int casesInMunicipality, int closedSchools ) {
+    	this.municipality = municipality;
+    	this.casesInMunicipality = casesInMunicipality;
+    	this.closedSchools = closedSchools;
+    	allMunicipalities.set(count, this);
+    	count++;
+    }
     	
-    	public String getMunicipality() {
-    		return municipality;
-    	}
+    public String getMunicipality() {
+    	return municipality;
+    }
     	
-    	public void setMunicipality(String municipality) {
-    		this.municipality = municipality;
-    	}
+    public int getCasesInMunicipality() {
+    	return casesInMunicipality;
+    }
     	
-    	public int getCasesinmunicipality() {
-    		return casesinmunicipality;
-    	}
+    public void setCasesInMunicipality(int casesInMunicipality) {
+    	this.casesInMunicipality = casesInMunicipality;
+    }
     	
-    	public void setCasesinmunicipality(int casesinmunicipality) {
-    		this.casesinmunicipality = casesinmunicipality;
-    	}
+    public int getClosedSchools() {
+    	return closedSchools;
+    }
     	
-    	public int getClosedschools() {
-    		return closedschools;
-    	}
+    public void setClosedSchools(int closedSchools) {
+    	this.closedSchools = closedSchools;
+    }
     	
-    	public void setClosedschools(int closedschools) {
-    		this.closedschools = closedschools;
-    	}
-    	
-  
-    	/*This method informs a municipality about whether or
-    	 * not one of its schools is closed
-    	 */
-    	static int id = -1;
-    	public static void makeAnnouncement( int id) {
-    		int i = 0;
-    		boolean flag1 = false;
-    		while (i < allmunicipalities.size() && flag1 == false) {
-    			int j = 0;
-    			boolean flag2 = false;
-    			while (j < allmunicipalities.get(i).schools.size() && flag2 == false) {
-    				if (allmunicipalities.get(i).schools.get(j).getSchoolid() == id &&  allmunicipalities.get(i).schools.get(j).getSchoolisclosed() == true) {
-    					System.out.println("Το σχολείο " + allmunicipalities.get(i).schools.get(j).getNameofschool() + "είναι κλειστό");
-    					allmunicipalities.get(i).closedschools ++;
+    /*This method informs a municipality about whether or
+    * not one of its schools is closed
+    */
+    static int id = -1;
+    public static void makeAnnouncement(int id) {
+    	int i = 0;
+    	boolean flag1 = false;
+    	while (i < allMunicipalities.size() && flag1 == false) {
+    		int j = 0;
+    		boolean flag2 = false;
+    		while (j < allMunicipalities.get(i).schools.size() && flag2 == false) {
+    			if (allMunicipalities.get(i).schools.get(j).getSchoolid() == id &&  allMunicipalities.get(i).schools.get(j).getSchoolisclosed() == true) {
+					System.out.println("The school " + allmunicipalities.get(i).schools.get(j).getNameofschool() + "is closed.");
+    					allmunicipalities.get(i).closedschools++;
     					flag2 = true;
     					flag1 = true;
-    				}
-    				j ++;
     			}
-    			i ++;
+    			j ++;
     		}
-    		
-    		
-    	}
+    		i ++;
+    	}		
+    }
 }
